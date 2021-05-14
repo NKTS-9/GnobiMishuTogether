8d75f2e8f6ce87b5d03c0c2ab29fabac49fd33cf
Akrolh <81387399+Akrolh@users.noreply.github.com>
2021-05-14 12:06:20 +0300

    Add files via upload

---
+++ lab_t11c/script.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+head -n 1 $"text.txt";
+tail -n 1 $"text.txt";
---
+++ lab_t11c/test.sh
@@ -0,0 +1,12 @@
+
+echo "Hello world";
+echo $USER;
+echo $HOME;
+echo "Не катайтесь на стульях";
+echo "а то будет";
+echo "очень";
+echo "очень";
+echo "очень";
+echo "очень";
+echo "ну очень плохо";
+echo "Богдану";
